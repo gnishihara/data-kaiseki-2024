3757047b901bd643544c8346cd7d322f19efe9fa
Greg Nishihara <gnishihara@gmail.com>
2024-09-23 13:48:17 +0900

    update

---
+++ day-01-pm.R
@@ -0,0 +1,18 @@
+# Excelファイルの読み込み
+# 2024-09-23
+# Greg Nishihara
+
+library(tidyverse)
+library(readxl)
+# 一番目のシート
+# 読み込み範囲 A1:I32
+
+fname = "Data/20231206シキシマハナダイ_dataset.xlsx"
+sheets = excel_sheets(fname)
+range = "A1:I32"
+
+fish_data = 
+  read_xlsx(path = fname,
+            sheet = sheets[1],
+            range = range)
+fish_data
