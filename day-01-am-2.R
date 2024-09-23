bcfcf34ed70b6ab6cc5755ab41d89b8b3304ca61
Greg Nishihara <gnishihara@gmail.com>
2024-09-23 11:31:15 +0900

    update

--- day-01-am-2.R
+++ day-01-am-2.R
@@ -21,6 +21,30 @@ fish_data = tibble(fish_loc, fish_id, fish_size)
 # <int>: integer, 整数
 fish_data
 
+# tidyverse におけるデータの抽出
+# |> CTRL + SHIFT + M パイプ演算子
+# %>% もパイプ演算子
 
+fish_data |> pull(fish_loc) # pull() は tibble の変数を返す
+fish_data$fish_loc # 28と同じ
+
+x = fish_data |> pull(fish_id)
+x
+
+# tibble のデータ操作
+# mutate() は新しい変数を作ってくれる
+fish_data = 
+  fish_data |> 
+  mutate(fish_size2 = fish_size / 100)
+fish_data
+
+fish_data = 
+  fish_data |> 
+  mutate(
+    fs = fish_size2^2,
+    invfs = 1 / fs
+  )
+
+fish_data
 
 
