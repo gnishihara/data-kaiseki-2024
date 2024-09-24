50e3be3626fe05bdf9b890e2457c8cfb450f9d88
Greg Nishihara <gnishihara@gmail.com>
2024-09-24 13:31:42 +0900

    update

---
+++ day-02-pm.R
@@ -0,0 +1,22 @@
+# Castaway CTD データの解析
+# 2024-09-24
+# Greg Nishihara
+
+# パッケージの読み込み
+
+library(tidyverse)
+library(lubridate) # 時刻データ処理
+
+# CTDファイルの下６桁の番号が 005000 以下のものが
+# ステーション１（桟橋の陸側）
+# その他はステーション２（桟橋の先端）
+# CTD の CSV ファイルは プロジェクトフォルダの
+# Data フォルダの CTD_Dataset フォルダにいれましょう。
+
+# データファイルのリスト作る
+# here::here() が動かない場合、here パッケージをインストールしましょう
+folder = here::here("Data/CTD_Dataset/") # データファイルへのパス
+dir(folder) # フォルダ内のファイルのリストを返す
+
+fnames = dir(folder, full.names = TRUE) # パスありリスト
+fnames
