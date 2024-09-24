d53abad651a22d4abd924515ea9f157ca94c6379
Greg Nishihara <gnishihara@gmail.com>
2024-09-24 15:10:28 +0900

    update

--- day-02-pm-2.R
+++ day-02-pm-2.R
@@ -3,8 +3,13 @@
 # Greg Nishihara
 
 # パッケージの読み込み
-library(tidyverse)
+library(tidyverse) # tidyverse（モダンR）
 library(lubridate) # 時刻データ処理
+
+library(magick)    # 画像処理用のパッケージ
+library(showtext)  # 作図用フォントのパッケージ
+library(ggtext)    # 作図用フォントのパッケージ
+
 
 # 保存済みデータファイルの読み込み
 fname = here::here("Output/CTD_dataset.csv")
