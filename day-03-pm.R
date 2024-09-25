9e9ae4db4dbeef303dc694490d5c00eb6e02c5c2
Greg Nishihara <gnishihara@gmail.com>
2024-09-25 13:06:41 +0900

    Update

---
+++ day-03-pm.R
@@ -0,0 +1,11 @@
+# 多重比較の紹介
+# 2024-09-25
+# Greg Nishihara
+
+
+# パッケージの読み込み
+
+library(tidyverse) # tidyverse
+library(lubridate) # 日時データの処理
+library(emmeans)   # 多重比較
+library(ggtext)    # ggplot にマークダウンを使う
