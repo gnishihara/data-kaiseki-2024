bf2c5ea2743f8b60d0ef5633275dce5c4b063935
Greg Nishihara <gnishihara@gmail.com>
2024-09-25 09:20:21 +0900

    Update

--- day-03-am.R
+++ day-03-am.R
@@ -14,18 +14,18 @@ library(magick)    # 画像処理 (imagemagick)
 # 長崎県新上五島町鯛ノ浦湾の海水温データ（ロガーは海底に設置した）
 fname = here::here("./Data/Temperature_Dataset/Temp_11000848_tainoura_0m_170614.csv")
 
+# CSVファイルの読み込み
 df1 = read_csv(fname, skip = 1)
 
+# 変数を選択して、変数名を変える
 df1 = df1 |> 
   select(datetime = `日付 時間, GMT+09:00`,
          temperature = matches("温度"))
-df1
 
+# 日時データを文字列から時刻データに変換する
 df1 = df1 |> 
   mutate(datetime = parse_date_time(datetime,
                                     orders = "%m/%d/%Y %I:%M:%S %p"))
-
-df1
 
 
 
