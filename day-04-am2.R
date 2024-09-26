051a173ccafe6d11ce02e1c506475af75add5898
Greg Nishihara <gnishihara@gmail.com>
2024-09-26 13:32:23 +0900

    Update

--- day-04-am2.R
+++ day-04-am2.R
@@ -281,7 +281,94 @@ ggplot(df1) +
                  y = Scruz30))
 
 
+############################################################
 
+# Scruzの中央値に対するlogAreaの結果
+
+qdata = df1 |> 
+  expand(logArea = seq(min(logArea), max(logArea), length = 21),
+         Scruz30 = median(Scruz30)) |> 
+  mutate(Area = exp(logArea),
+         Scruz = Scruz30 * 30)
+tmp = predict(m3, newdata = qdata, se.fit = TRUE) |> as_tibble()
+qdata = bind_cols(qdata, tmp)
+
+# Areaの中央値に対するScruz30の結果
+
+rdata = df1 |> 
+  expand(Area = median(Area),
+         Scruz30 = seq(min(Scruz30), max(Scruz30), length = 21)) |> 
+  mutate(Scruz = Scruz30 * 30,
+         logArea = log(Area))
+tmp = predict(m3, newdata = rdata, se.fit = TRUE) |> as_tibble()
+rdata = bind_cols(rdata, tmp)
+
+
+
+# モデルとデータの図 
+xtitle = "log Area (log(m<sup>2</sup>))" # markdown 
+ytitle = "Species (-)"
+# Scruzの中央値に対するlogArea との関係を示す図。
+# 線はモデル結果（期待値）、グレーの範囲は期待値の95%信頼区間。
+ggplot(df1) +
+  geom_ribbon(aes(x = logArea,
+                  ymin = exp(fit - se.fit),
+                  ymax = exp(fit + se.fit)),
+              data = qdata,
+              alpha = 0.5) +
+  geom_line(aes(x  = logArea, y = exp(fit)), 
+            data = qdata) +
+  geom_point(aes(x = logArea, y = Species)) +
+  scale_x_continuous(name = xtitle,
+                     limits = c(-5, 10),
+                     breaks = seq(-5, 10, by = 5)) +
+  scale_y_continuous(name = ytitle,
+                     limits = c(0, 650),
+                     breaks = seq(0, 650, by = 130)) +
+  theme(
+    axis.title.x = element_markdown(),
+    axis.title.y = element_markdown(),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank(),
+    axis.ticks.x = element_line(color = "black"),
+    axis.ticks.y = element_line(color = "black"),
+    panel.border = element_rect(fill = NA, color = "black"),
+  )
+
+xtitle2 = "Distance to Santa Cruz (km / 30)"
+ggplot(df1) +
+  geom_ribbon(aes(x = Scruz30,
+                  ymin = exp(fit - se.fit),
+                  ymax = exp(fit + se.fit)),
+              data = rdata,
+              alpha = 0.5) +
+  geom_line(aes(x = Scruz30, y = exp(fit)),
+            data = rdata) + 
+  geom_point(aes(x = Scruz30, y = Species)) +
+  scale_x_continuous(name = xtitle2,
+                     limits = c(0, 10),
+                     breaks = seq(0, 10, by = 2)) +
+  scale_y_continuous(name = ytitle,
+                     limits = c(0, 650),
+                     breaks = seq(0, 650, by = 130)) +
+  theme(
+    axis.title.x = element_markdown(),
+    axis.title.y = element_markdown(),
+    axis.line.x = element_blank(),
+    axis.line.y = element_blank(),
+    axis.ticks.x = element_line(color = "black"),
+    axis.ticks.y = element_line(color = "black"),
+    panel.border = element_rect(fill = NA, color = "black"),
+  )
+
+
+ggplot(df1) + 
+  geom_tile(aes(x = logArea,
+                y = Scruz30,
+                fill = exp(fit)), 
+            data = pdata) + 
+  geom_point(aes(x = logArea,
+                 y = Scruz30))
 
 
 
