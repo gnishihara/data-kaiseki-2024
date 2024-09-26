d6d381ccedbff2fef80a6ffb514d4fde165ea005
Greg Nishihara <gnishihara@gmail.com>
2024-09-26 09:49:42 +0900

    Update

--- day-04-am.R
+++ day-04-am.R
@@ -63,3 +63,58 @@ plot3 =
 # ここでは patchwork の演算子を使って、上の図を結合する
 plot1 + plot2 + plot3 + plot_layout(ncol = 2, nrow = 2)
 
+# 左上の図：fit が上昇すると残渣が下がって、そのあと広がる。
+# 等分散性ではない、残渣にパターンが存在する。
+# 
+# 右上の図：fit が上昇すると、標準化残渣も上昇する。
+# 明らかに、等分散性ではない。
+# 
+# 左下の図：残渣の四分位数は線の上にのるので、正規性に大きな
+# 問題はない。
+
+# 説明の上限を１０あたりにしたい
+log(4000)
+df1 = df1 |> 
+  mutate(logAdj = log(Adjacent),
+         logArea = log(Area),
+         logElev = log(Elevation),
+         Near5 = Nearest / 5,
+         Scruz30 = Scruz / 30)
+
+# cols = -Species: Species 以外の変数を縦に結合する
+df1 |> 
+  select(Species, logAdj, logArea, logElev, Near5, Scruz30) |> 
+  pivot_longer(cols = -Species) |> 
+  ggplot() + 
+  geom_point(aes(x = value, y = Species, color = name)) +
+  facet_wrap(vars(name), scales = "free")
+
+# 帰無仮説：
+# （１）Species と log(Adjacent) との関係はない
+# （２）Species と log(Area) との関係はない
+# （３）Species と log(Elevation) との関係はない
+# （４）Species と Nearest/5 との関係はない
+# （５）Species と Scruz/30 との関係はない
+
+m2 = glm(Species ~ logAdj + logArea + logElev + Near5 + Scruz30,
+         data = df1, family = gaussian("identity"))
+
+summary(m2) # 一般化線形モデルの係数表
+
+# モデルの診断図
+# 残渣、期待値、標準化残渣の絶対値の平方根
+df3 = df1 |> 
+  select(Species) |> 
+  mutate(residual = residuals(m2),
+         fit = predict(m2)) |> 
+  mutate(stdresid  = sqrt(abs(rstandard(m2))))
+
+plot1 = ggplot(df3) + geom_point(aes(x = fit, y = residual))
+plot2 = ggplot(df3) + geom_point(aes(x = fit, y = stdresid))
+plot3 = 
+  ggplot(df3) +
+  geom_qq(aes(sample = residual)) +
+  geom_qq_line(aes(sample = residual))
+
+# ここでは patchwork の演算子を使って、上の図を結合する
+plot1 + plot2 + plot3 + plot_layout(ncol = 2, nrow = 2)
