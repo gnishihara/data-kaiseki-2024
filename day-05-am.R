8068dba3cdb7c146561eb77a55a35a8aea083e81
Greg Nishihara <gnishihara@gmail.com>
2024-09-27 09:45:29 +0900

    Update readme

--- day-05-am.R
+++ day-05-am.R
@@ -61,6 +61,65 @@ m1 = nls(uptake ~ uptake_model(x = conc,
 
 summary(m1)
 
+# Quebec, nonchilled
+# Quebec, chilled
+# Mississippi, nonchilled
+# Mississippi, chilled
+
+tmp = df1 |> 
+  filter(str_detect(Type, "Que")) |> 
+  filter(str_detect(Treatment, "nonchill"))
+
+t1 = nls(uptake ~ uptake_model(x = conc,
+                               p1 = p1, 
+                               p2 = p2, 
+                               p3 = p3),
+         data = tmp,
+         start = list(p1 = 10, p2 = 30/250, p3 = 30))
+
+summary(t1)
+
+# フルモデル (Treatment, Type ごとに係数を求める)
+
+df1 = df1 |> 
+  group_nest(Type, Treatment) |> 
+  mutate(i = 1:4, .before = Type) |> 
+  unnest(data)
+
+m2 = nls(uptake ~ uptake_model(x = conc,
+                          p1 = p1[i],
+                          p2 = p2[i],
+                          p3 = p3[i]),
+    data = df1,
+    start = list(p1 = rep(10, 4),
+                 p2 = rep(30/250, 4),
+                 p3 = rep(30, 4)))
+
+summary(m2)
+
+# モデル2（Treatment ごとの係数）
+df1 = df1 |> 
+  group_nest(Treatment) |> 
+  mutate(j = 1:2) |> unnest(data)
+
+m2 = nls(uptake ~ uptake_model(x = conc,
+                               p1 = p1[j],
+                               p2 = p2[j],
+                               p3 = p3[j]),
+         data = df1,
+         start = list(p1 = rep(10, 2),
+                      p2 = rep(30/250, 2),
+                      p3 = rep(30, 2)))
+
+summary(m2)
+
+# モデル3（Type ごとの係数）
+
+
+
+
+
+
 
 
 
