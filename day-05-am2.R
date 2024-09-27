f06f68b0e717759a3a6d5cabf30445c1be219da8
Greg Nishihara <gnishihara@gmail.com>
2024-09-27 11:27:24 +0900

    Update readme

--- day-05-am2.R
+++ day-05-am2.R
@@ -1,3 +1,10 @@
+# 守岡くんのデータの再解析
+# GLM を用いた解説
+# 2024-09-27
+# Greg Nishihara
+
+
+
 # パッケージの読み込み
 
 library(tidyverse)
@@ -30,14 +37,14 @@ df1 = df1 |>
          id = matches("元番"))
 
 ggplot(df1) + 
-  geom_point(aes(x = class, y = length))
+  geom_boxplot(aes(x = class, y = length))
 
 
 ggplot(df1) + 
   geom_point(aes(x = width, y = length,
                  color = class))
 
-
+# 正規分布を仮定してい glm あてはめた ######################
 m0 = glm(length ~ 1, data = df1)
 m1 = glm(length ~ width , data = df1)
 m2 = glm(length ~ class, data = df1)
@@ -45,13 +52,13 @@ m3 = glm(length ~ width + class, data = df1)
 m4 = glm(length ~ width * class, data = df1)
 AIC(m0, m1, m2, m3, m4)
 
-m3
+m3 # AIC が最も低い
 
 df1 = df1 |> mutate(residuals = residuals(m3),
               fit = predict(m3),
               stdresid = sqrt(abs(scale(residuals)[,1])))
 
-
+# 診断図を確認する
 p1 = ggplot(df1) + geom_point(aes(x = fit, y = residuals))
 p2 = ggplot(df1) + geom_point(aes(x = fit, y = stdresid))
 p3 = ggplot(df1) + 
@@ -60,7 +67,8 @@ p3 = ggplot(df1) +
 
 p1 + p2 + p3 + plot_layout(nrow = 2, ncol = 2)
 
-#
+# 正規分布が適していないので、ガンマ分布をためす。
+# 
 
 
 m0 = glm(length ~ 1, data = df1, family = Gamma("log"))
@@ -69,7 +77,10 @@ m2 = glm(length ~ class, data = df1, family = Gamma("log"))
 m3 = glm(length ~ width + class, data = df1, family = Gamma("log"))
 m4 = glm(length ~ width * class, data = df1, family = Gamma("log"))
 AIC(m0, m1, m2, m3, m4)
-m4
+
+m4　# AIC がもっとも低い
+
+m4_gamma = m4
 
 df1 = df1 |> mutate(residuals = statmod::qresiduals(m4),
                     fit = predict(m4),
@@ -87,5 +98,48 @@ p1 + p2 + p3 + plot_layout(nrow = 2, ncol = 2)
 
 
 
+
+# width と length のログ変換解析
+
+ggplot(df1) + 
+  geom_point(aes(x = log(width), y = log(length),
+                 color = class))
+
+
+
+# 正規分布を仮定してい glm あてはめた ######################
+
+df1 = df1 |> 
+  mutate(logL = log(length),
+         logW = log(width))
+
+m0 = glm(logL ~ 1, data = df1)
+m1 = glm(logL ~ logW , data = df1)
+m2 = glm(logL ~ class, data = df1)
+m3 = glm(logL ~ logW + class, data = df1)
+m4 = glm(logL ~ logW * class, data = df1)
+AIC(m0, m1, m2, m3, m4)
+
+m3 # AIC が最も低い
+
+df1 = df1 |> mutate(residuals = residuals(m3),
+                    fit = predict(m3),
+                    stdresid = sqrt(abs(scale(residuals)[,1])))
+
+# 診断図を確認する
+p1 = ggplot(df1) + geom_point(aes(x = fit, y = residuals))
+p2 = ggplot(df1) + geom_point(aes(x = fit, y = stdresid))
+p3 = ggplot(df1) + 
+  geom_qq(aes(sample = residuals)) +
+  geom_qq_line(aes(sample = residuals))
+
+p1 + p2 + p3 + plot_layout(nrow = 2, ncol = 2)
+
+
+m3
+m4_gamma
+AIC(m3, m4_gamma)  # m3
+
+summary(m3)
 
 
